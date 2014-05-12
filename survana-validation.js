afa370680cce04cf739bc3ac15c5f66d6047aa9f
Victor Petrov <victor.petrov@gmail.com>
2014-05-11 21:58:26 -0400

    Update description of Survana.Validation.Validate()

--- survana-validation.js
+++ survana-validation.js
@@ -147,10 +147,11 @@ if (!window.Survana) {
 
     /**
      * Validate <form> elements, based on a validation configuration pre-built when publishing the form
-     * and custom validation messages
+     * and custom validation messages. Returns all validated responses.
      * @param form The HTMLFormElement being validated
      * @param config (optional) Validation configuration
      * @param messages (optional) Custom error messages
+     * @return {Object|Boolean} Returns all validated responses as an Object, or false if validation failed
      */
     Survana.Validation.Validate = function (form, config, messages) {
 
